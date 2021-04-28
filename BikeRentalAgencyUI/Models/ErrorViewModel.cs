dc356d6914a0eec860212d9caff65eeb8ecfd7d1
LAPTOP-L8E5G43E\Nikki <kimberlyretherford@icloud.com>
2021-04-28 08:24:45 -0400

    Added WebApp

---
+++ BikeRentalAgencyUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BikeRentalAgencyUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
